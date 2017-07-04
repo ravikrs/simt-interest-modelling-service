59468b6e02fcf4781248eaca7ea66edf997559d0
Ravi Kumar Singh <ravi.kumar.singh@kex-ag.com>
2017-07-04 17:01:25 +0200

    update run script

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-mvn clean install -Dmaven.test.skip=true & java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -Xms1024M -Xmx30072M -jar target/cimt-keyphrase-extraction-0.0.1-SNAPSHOT.jar 
+mvn clean install -Dmaven.test.skip=true & java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -Xms1024M -Xmx30072M -jar target/simt-interest-modelling-service-0.0.1-SNAPSHOT.jar
